65874dfef64534cd235733ea397024af8ff411f9
Surajkandel <107171189+Surajkandel@users.noreply.github.com>
2023-06-26 15:09:39 +0545

    Add files via upload

---
+++ admin/Manage-Doner.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <title>
+            Blood bank management system - Home page
+        </title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        
+        <!-- menu section starts -->
+        <div class="menu">
+            <div class="wrapper">
+                <ul>
+                    <li> <a href="index.php">Home</a></li>
+                    <li> <a href="Manage-Clients.php">Clients</a></li>
+                    <li> <a href="Manage-Doner.php">Doner</a></li>
+                    <li> <a href="Manage-Patient.php">Patient</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+
+        <!-- menu section ends -->
+
+        <!-- main content section starts -->
+        <div class="main-content">
+            <div class="wrapper">
+                <strong>THIS IS doner PAGE</strong>
+                <div>
+
+                </div>
+            </div>
+        </div>
+
+
+        <!-- main content section ends -->
+
+        <!-- footer section starts -->
+        <div class="footer">
+        <div class="wrapper">
+                <p class = "text-center">2023 © ALL RIGHT RESERVED</p>
+            </div>
+        </div>
+
+
+
+
+        <!-- footer section ends -->
+    </body>
+
+</html>
