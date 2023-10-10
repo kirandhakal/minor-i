169f69667f9b650a761f7e163a256658e2fd2bdb
kirandhakal <116267720+kirandhakal@users.noreply.github.com>
2023-10-10 21:57:10 +0545

    Add files via upload

--- admin/Manage-Clients.php
+++ admin/Manage-Clients.php
@@ -1,52 +1,95 @@
-<html>
-    <head>
-        <title>
-            Blood bank management system - Home page
-        </title>
-        <link rel="stylesheet" href="../css/admin.css">
-    </head>
-    <body>
-        
-        <!-- menu section starts -->
-        <div class="menu">
-            <div class="wrapper">
-                <ul>
-                    <li> <a href="index.php">Home</a></li>
-                    <li> <a href="Manage-Clients.php">Clients</a></li>
-                    <li> <a href="Manage-Doner.php">Doner</a></li>
-                    <li> <a href="Manage-Patient.php">Patient</a></li>
-                </ul>
-            </div>
+<?php include('partials/menu.php') ?>
+
+
+    
+
+<!-- main content section starts -->
+<div class="main-content">
+    <div class="wrapper">
+        <strong>THIS IS client PAGE</strong>
+        <!-- <br><br>
+        <div><a href="#" class="btn-primary">Add client</a></div>
+        <br> -->
+        <div class="whole">
+            <table class = "tbl">
+                <tr>
+                    <th>S.N</th>
+                    <th>I.D</th>
+                    <th>Full Name</th>
+                    <th>User Name</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    
+              
+
+                <?php
+
+                //create sql to get the information of clients from database table
+                $sql = "SELECT * FROM tbl_signup";
+
+                //Execute the query
+                $res = mysqli_query($conn, $sql);
+
+                //check weather the query is executed or not
+                if($res==true)
+                {
+                    //count the number of rows to check the number of datas
+                    $count = mysqli_num_rows($res);
+
+                    // initilizing the value for serialno (sn)
+                    $sn=1;
+
+                    // get the values from the rows 
+                    if($count>0)
+                    {
+                        //use loop to get all the data from database
+                        while($rows = mysqli_fetch_assoc($res))
+                        {
+
+                            // get individual data
+                            $id = $rows['id'];
+                            $fullname = $rows['Full_name'];
+                            $username = $rows['User_name'];
+                            $email = $rows['Email'];
+                            $password = $rows['Password'];
+                            
+
+                            //dispaly the value in the table
+                            ?>
+                            <tr>
+                                <td><?php echo $sn++?></td>
+                                <td><?php echo $id?></td>
+                                <td><?php echo $fullname ?></td>
+                                <td><?php echo $username ?></td>
+                                <td><?php echo $email ?></td>
+                                <td><?php echo $password ?></td>
+
+
+                            </tr>
+                            <?php
+
+                        }
+                    }
+                    else{
+                        //no data in database
+                    }
+                }
+
+
+                ?>
+                
+                
+            </table>
+
         </div>
-
-
-
-        <!-- menu section ends -->
-
-        <!-- main content section starts -->
-        <div class="main-content">
-            <div class="wrapper">
-                <strong>THIS IS client PAGE</strong>
-                <div>
-
-                </div>
-            </div>
-        </div>
-
-
-        <!-- main content section ends -->
-
-        <!-- footer section starts -->
-        <div class="footer">
-        <div class="wrapper">
-                <p class = "text-center">2023 © ALL RIGHT RESERVED</p>
-            </div>
-        </div>
+    </div>
+</div>
 
 
 
 
-        <!-- footer section ends -->
-    </body>
+<!-- main content section ends -->
 
-</html>
+       
+
+<?php include('partials/footer.php') ?>
